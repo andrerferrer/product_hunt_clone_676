bd57e576f925e5a9789dffdba39e762582be45fa
andre ferrer <andrerferrer@hotmail.com>
2021-08-26 03:06:43 -0300

    Create products new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
+
+  resources :products, only: %i[ new create ]
   
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,10 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
+  # Devise test helpers
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -11,3 +15,11 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+# Register the new driver for Capybara
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+Capybara.save_path = Rails.root.join('tmp/capybara')
+Capybara.javascript_driver = :headless_chrome
